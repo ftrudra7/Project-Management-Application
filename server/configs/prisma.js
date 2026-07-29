55496de1ac5778e602e653152b3f561031e1b90f
ftrudra7 <rudrajha800@gmail.com>
2026-07-29 20:00:42 +0530

    Update Prisma

--- server/configs/prisma.js
+++ server/configs/prisma.js
@@ -6,4 +6,19 @@ import { neonConfig } from '@neondatabase/serverless';
 import ws from 'ws';
 neonConfig.webSocketConstructor = ws;
 
-//To work in edge enviornments (Cloudflare workers, Vercel Edge, etc.), enable querying
+//To work in edge enviornments (Cloudflare workers, Vercel Edge, etc.), enable querying over fetch
+// neonConfig.poolQueryViaFetch = true
+
+//Type definitions
+//declare global {
+//  var prisma: PrismaClient | undefined
+// }
+
+const connectionString = '${process.env.DATABASE_URL}';
+
+const adapter = new PrismaNeon({ connectionString });
+const prisma = global.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV === 'development') 
+
+export default prisma;
