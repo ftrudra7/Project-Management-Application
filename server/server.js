d2352aaf75d14148fa0022d841affb29ac3245b1
ftrudra7 <rudrajha800@gmail.com>
2026-08-03 11:41:40 +0530

    Add middlewares and protect function

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import { clerkMiddleware } from '@clerk/express';
 import { serve } from "inngest/express";
 import { functions, inngest } from './inngest/index.js';
 import workspaceRouter from './routes/workspaceRoutes.js';
+import { protect } from './middlewares/authMiddleware.js';
 
 const app = express();
 
@@ -17,7 +18,8 @@ app.get('/', (req, res) => res.send('Server is live!'));
 app.use("/api/inngest", serve({ client: inngest, functions}));
 
 //Routes
-app.use("/api/workspaces", workspaceRouter)
+app.use("/api/workspaces", protect, workspaceRouter)
+
 
 const PORT = process.env.PORT || 5000;
 
